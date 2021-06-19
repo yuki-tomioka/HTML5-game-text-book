8c792151bc2f07d8af9d3b861d6c0cb21b753995
Yuki Tomioka <yuki.tomioka@zozo.com>
2021-06-19 17:46:15 +0900

    webkpackコマンドの実行

---
+++ www/test_npm.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.test_npm=o():e.test_npm=o()}(self,(function(){return console.log(3),{}}));

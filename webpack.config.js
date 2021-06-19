521b84b64b3283452f5bc367dc5999064e5b9e85
Yuki Tomioka <yuki.tomioka@zozo.com>
2021-06-19 17:45:16 +0900

    webkpack.config.jsの追加

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = (env, args) => {
+    return {
+        mode: 'production',
+        entry: {
+            index: path.join(__dirname, 'index.ts'),
+        },
+
+        output: {
+            path: path.join(__dirname, 'www'),
+            filename: 'test_npm.js',
+            library: 'test_npm',
+            libraryTarget: 'umd',
+        },
+
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                use: [{ loader: 'ts-loader' }]
+            }]
+        }
+    }
+};
